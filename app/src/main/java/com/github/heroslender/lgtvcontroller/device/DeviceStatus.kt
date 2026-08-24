ebe818c5d3e8457bb881f996a9a39b0f07f27d70
Tuantai17 <tuantainguyen13579@gmai.com>
2026-08-24 17:24:54 +0700

    Initial commit

---
+++ app/src/main/java/com/github/heroslender/lgtvcontroller/device/DeviceStatus.kt
@@ -0,0 +1,13 @@
+package com.github.heroslender.lgtvcontroller.device
+
+import com.github.heroslender.lgtvcontroller.R
+
+enum class DeviceStatus(
+    val nameResId: Int
+) {
+    CONNECTING(R.string.connecting),
+    PAIRING(R.string.pairing),
+    CONNECTED(R.string.connected),
+    DISCONNECTED(R.string.disconnected),
+    PIN_REQUIRED(R.string.pin_required)
+}
